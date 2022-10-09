208dc1e04511733567d034b184f0ccf7072768d4
sergeymotrenko <VTB4038040@region.vtb.ru>
2022-10-09 20:32:24 +0300

    delete unnecessary files and configs, fix liquibase migration problem

--- src/main/java/org/testmanager/repository/UserRepository.java
+++ src/main/java/org/testmanager/repository/UserRepository.java
@@ -3,6 +3,6 @@ package org.testmanager.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.testmanager.model.entity.User;
 
-public interface UsersRepository  extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
 
 }
